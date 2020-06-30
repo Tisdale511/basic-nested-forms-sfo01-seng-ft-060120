246e544cb8db0df02b0a4d010d58bcc7d293d054
Tisdale511 <tisdalefry@gmail.com>
2020-06-30 14:18:25 -0700

    Done.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ActiveRecord::Base
   belongs_to :person
+
+  def artist_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist[:name])
+    self.artist.update(artist)
+  end
+  
 end
